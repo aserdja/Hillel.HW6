fc98298a205938cd81a38d0d6fa7b1f37d06ac5e
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-08-21 00:38:21 +0300

    Created StudentList class and methods

---
+++ HW6/ListPractiseLibrary/StudentList.cs
@@ -0,0 +1,42 @@
+﻿namespace ListPractiseLibrary
+{
+	public class StudentList
+	{
+		private List<string> students = new();
+
+		public void Add(string studentFullName)
+		{
+			students.Add(studentFullName);
+            Console.WriteLine($"Student: {studentFullName} was added!");
+			ShowCount();
+        }
+
+		public void Remove(string studentFullName)
+		{
+			if (students.Remove(studentFullName))
+			{
+                Console.WriteLine($"Student: {studentFullName} was deleted!");
+				ShowCount();
+            }
+			else
+			{
+                Console.WriteLine($"Student {studentFullName} wasn't found!");
+            }
+		}
+
+		public void Find(string textToSearch)
+		{
+            Console.WriteLine($"Search result for \"{textToSearch}\":");
+            foreach (var student in students.FindAll(x => x.Contains(textToSearch)))
+			{
+                Console.WriteLine($"Student: {student}");
+            }
+            Console.WriteLine($"Total: {students.FindAll(x => x.Contains(textToSearch)).Count} matches!");
+        }
+
+		private void ShowCount()
+		{
+			Console.WriteLine($"Total: {students.Count} students in list!");
+		}
+	}
+}

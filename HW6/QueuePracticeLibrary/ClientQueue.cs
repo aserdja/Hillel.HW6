0d1de7e0f58770b75782d52dd832a79b185faeb2
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-08-22 17:06:31 +0300

    Created ClientQueue class and methods

---
+++ HW6/QueuePracticeLibrary/ClientQueue.cs
@@ -0,0 +1,31 @@
+﻿namespace QueuePracticeLibrary
+{
+	public class ClientQueue
+	{
+		private Queue<string> clientQueue = new();
+
+		public void Add(string name)
+		{
+			clientQueue.Enqueue(name);
+            Console.WriteLine($"Client {name} joined the queue!");
+        }
+
+		public string Remove()
+		{
+			string? result = "";
+			if (clientQueue.TryPeek(out result))
+			{
+				Console.WriteLine($"Client {result} currently in service!");
+				return clientQueue.Dequeue();
+			}
+
+            Console.WriteLine($"Queue is empty!");
+            return "";
+		}
+
+		public void GetQuantity()
+		{
+			Console.WriteLine($"There are currently {clientQueue.Count} clients in the queue!");
+        }
+	}
+}
